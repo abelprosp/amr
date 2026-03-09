b44b1c04f95172a9b9c3c88bf96e0535e5e5df94
abelprosp <prospettcomercial@gmail.com>
2026-03-09 08:51:15 -0300

    jkfekwdwqwdq

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   transpilePackages: ['unpdf'],
   webpack: (config, { dir }) => {
     config.resolve ??= {};
